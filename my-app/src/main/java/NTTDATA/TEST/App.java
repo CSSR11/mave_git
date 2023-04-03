1d3b4e75181dafcf1fe16e4bb39727741e436df0
cristian <cserrano7411@gmail.com>
2023-04-03 18:45:12 +0200

    Segunda version

---
+++ my-app/src/main/java/NTTDATA/TEST/App.java
@@ -0,0 +1,13 @@
+package NTTDATA.TEST;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
